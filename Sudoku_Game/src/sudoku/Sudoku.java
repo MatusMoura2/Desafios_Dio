0351fcb98bc0e52e278e4f292974bccf0c646c9f
MatusMoura2 <matus.moura333x2@gmail.com>
2025-06-21 02:27:18 -0300

    ajustando painel

--- Sudoku_Game/src/sudoku/Sudoku.java
+++ Sudoku_Game/src/sudoku/Sudoku.java
@@ -1,8 +1,11 @@
 package sudoku;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Sudoku {
 	
@@ -10,6 +13,9 @@ public class Sudoku {
 	int boardHeight = 650;
 	
 	JFrame frame = new JFrame("SUDOKU GAME");
+	JLabel textLabel = new JLabel();
+	JPanel textPanel = new JPanel();
+	
 	
 	public Sudoku(){
 		frame.setVisible(true);
@@ -19,5 +25,11 @@ public class Sudoku {
 		frame.setLocationRelativeTo(null);
 		frame.setLayout(new BorderLayout());
 		
+		textLabel.setFont(new Font("Arial",Font.BOLD,30));
+		textLabel.setHorizontalAlignment(JLabel.CENTER);
+		textLabel.setText("Sudoku: 0");
+		
+		textPanel.add(textLabel);
+		frame.add(textPanel, BorderLayout.NORTH);		
 	}
 }
