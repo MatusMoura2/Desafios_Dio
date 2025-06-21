6f5c84b7c30aa3546ec76a5b631ef17c5acc03b3
MatusMoura2 <matus.moura333x2@gmail.com>
2025-06-21 02:07:40 -0300

    criando o painel do game

--- Sudoku_Game/src/sudoku/Sudoku.java
+++ Sudoku_Game/src/sudoku/Sudoku.java
@@ -1,6 +1,23 @@
 package sudoku;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 public class Sudoku {
 	
+	int boardWidth = 600;
+	int boardHeight = 650;
 	
+	JFrame frame = new JFrame("SUDOKU GAME");
+	
+	public Sudoku(){
+		frame.setVisible(true);
+		frame.setSize(boardWidth, boardHeight);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setLayout(new BorderLayout());
+		
+	}
 }
