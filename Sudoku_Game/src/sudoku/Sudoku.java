06be5be8f67b684e80f69f896a697c79a582a8b2
MatusMoura2 <matus.moura333x2@gmail.com>
2025-06-22 14:40:31 -0300

    iniciando grid com numeros

--- Sudoku_Game/src/sudoku/Sudoku.java
+++ Sudoku_Game/src/sudoku/Sudoku.java
@@ -2,34 +2,68 @@ package sudoku;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Sudoku {
-	
+	class Tille extends JButton {
+		int rowNumber;
+		int columnNumber;
+
+		public Tille(int rowNumber, int columnNumber) {
+			super();
+			this.rowNumber = rowNumber;
+			this.columnNumber = columnNumber;
+		}
+
+	}
+
 	int boardWidth = 600;
 	int boardHeight = 650;
-	
+
+	String[] puzzle = { "--74916-5", "2---6-3-9", "-----7-1-", "-586---4-", "---3--9--", "--62-187-", "9-4-7---2",
+			"67-83----", "81--45---" };
+
+	String[] solution = { "387491625", "241568379", "569327418", "758612934", "123784596", "496253187", "934176852",
+			"675832941", "812945763" };
+
 	JFrame frame = new JFrame("SUDOKU GAME");
 	JLabel textLabel = new JLabel();
 	JPanel textPanel = new JPanel();
-	
-	
-	public Sudoku(){
+	JPanel boardPanel = new JPanel();
+
+	public Sudoku() {
 		frame.setVisible(true);
 		frame.setSize(boardWidth, boardHeight);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setLayout(new BorderLayout());
-		
-		textLabel.setFont(new Font("Arial",Font.BOLD,30));
+
+		textLabel.setFont(new Font("Arial", Font.BOLD, 30));
 		textLabel.setHorizontalAlignment(JLabel.CENTER);
 		textLabel.setText("Sudoku: 0");
-		
+
 		textPanel.add(textLabel);
-		frame.add(textPanel, BorderLayout.NORTH);		
+		frame.add(textPanel, BorderLayout.NORTH);
+
+		boardPanel.setLayout(new GridLayout(9, 9));
+		setupTiles();
+		frame.add(boardPanel,BorderLayout.CENTER);
+	}
+
+	void setupTiles() {
+		for (int r = 0; r < 9; r++) {
+			for (int c = 0; c < 9; c++) {
+				Tille tille = new Tille(r, c);
+				char tilleChar = puzzle[r].charAt(c);
+				tille.setText(String.valueOf(tilleChar));
+				boardPanel.add(tille);
+			}
+		}
 	}
 }
